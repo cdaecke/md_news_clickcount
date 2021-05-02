fa36b6b1f2c0c47f292014c7c0b5dc9cca679726
chris <typo3@mediadreams.org>
2021-05-02 22:27:37 +0200

    [TASK] add translation for orderBy lable in news plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,5 +18,15 @@ call_user_func(
             ['switchableControllerActions']
             ['newItems']
             ['News->mdIncreaseCount'] = 'LLL:EXT:md_news_clickcount/Resources/Private/Language/locallang.xlf:controllerAction.mdIncreaseCount';
+
+        /**
+         * Add translations to news `locallang_be`
+         */
+        $GLOBALS['TYPO3_CONF_VARS']
+            ['SYS']
+            ['locallangXMLOverride']
+            ['EXT:news/Resources/Private/Language/locallang_be.xlf']
+            [] = 'EXT:md_news_clickcount/Resources/Private/Language/Overrides/news_locallang_be.xlf';
+
     }
 );
