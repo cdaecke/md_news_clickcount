c9d06d1fb6af8abe76c67105f9c11fd7250e301d
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-01-15 07:47:05 +0100

    [TASK] Allow TYPO3 14 (#16)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-14.4.99',
             'news' => '11.0.0-13.9.99',
         ],
         'conflicts' => [],
